1ff5aaabecc7a604e87e4764e07c2476cfb8d64d
radicaldave <dbmailboxdb@gmail.com>
2014-05-14 16:56:02 +0200

    Ok cool
    whé

--- wp-content/themes/html5blank-master/header.php
+++ wp-content/themes/html5blank-master/header.php
@@ -4,6 +4,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
+		<link rel="stylesheet" type="text/css" href="styles.css">
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
@@ -32,12 +33,7 @@
 			<header class="header clear" role="banner">
 
 					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
+					
 					<!-- /logo -->
 
 					<!-- nav -->
--- wp-content/themes/html5blank-master/page.php
+++ wp-content/themes/html5blank-master/page.php
@@ -4,16 +4,12 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?></h1>
-
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
 
 				<br class="clear">
 
